3afdc11026ea0081d41814ea939af06dd5dd014f
riyaverma338 <vermariya1202@gmail.com>
2021-07-09 12:06:45 +0530

    footer

---
+++ script.js
@@ -0,0 +1,10 @@
+ const myButton = document.getElementById("myBtn");
+
+     onclick = function(){
+         scrollFunction()
+    };
+
+    function scrollFunction(){
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
