319dba326b267be0fb0fff95693442aa578c35e0
riyaverma338 <vermariya1202@gmail.com>
2021-07-23 15:14:05 +0530

    updates

--- script.js
+++ script.js
@@ -1,10 +1,56 @@
- const myButton = document.getElementById("myBtn");
+//SCROLL TO TOP
+var scrolltoTopButton = document.getElementById("myBtn");
 
-     onclick = function(){
-         scrollFunction()
+scrolltoTopButton.addEventListener("click", scrollToTop);
+
+function scrollToTop(){
+
+window.scrollTo({
+    top:0,
+    behavior: "smooth"
+});
+}
+
+//TYPING EFFECT
+const words = ["Hi, I'm Riya" , "I'm glad you're here!"];
+let i=0;
+let timer;
+function typingEffect() {
+    let head = words[i].split("");
+    var loopTyping = function() {
+        if(head.length > 0) {
+            document.getElementById('head').innerHTML += head.shift();
+        }
+        else {
+            deletingEffect();
+            return false;
+        };
+        timer = setTimeout(loopTyping, 250);
     };
+    loopTyping();
+}
 
-    function scrollFunction(){
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    }
+function deletingEffect(){
+    let head = words[i].split("");
+    var loopDeleting = function() {
+        if (head.length > 0) {
+            head.pop();
+
+            document.getElementById('head').innerHTML = head.join("");
+        }
+        else{
+                if (words.length > (i + 1)) {
+                    i++;
+                } else {
+                    i = 0;
+                };
+                typingEffect();
+                return false;
+            };
+            timer = setTimeout(loopDeleting, 50);
+        };
+        loopDeleting();
+    };
+    
+    typingEffect();
+
