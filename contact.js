32d1fa2040a69de7bd7fddab7720d7dd4a96603d
riyaverma338 <vermariya1202@gmail.com>
2021-09-02 23:48:13 +0530

    complete

--- contact.js
+++ contact.js
@@ -1,22 +1,16 @@
-function sendEmail(para){
+function sendEmail(){
     var tempPara ={
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
         subject: document.getElementById("subject").value,
         message: document.getElementById("message").value,
     };
-
-//YOU CAN PUT THIS INSIDE AN IF STATEMENT WHEN ADDING VALIDATIONS
     emailjs.send('service_yhz4w54', 'template_39k4vj5', tempPara)
-      .then(function(res){
-          console.log("success",res.status);
-      })
-  }
+    .then(function(res){
+        console.log("success",res.status);
+    })
+}
 
 
-//   emailjs.send('service_yhz4w54', 'template_39k4vj5', tempPara)
-//       .then((result) => {
-//           console.log(result);
-//       }, (error) => {
-//           console.log(error.text);
-//       });
+
+
