8ad9e8a03d6df341f8e1376cb778bcfc704cec06
riyaverma338 <vermariya1202@gmail.com>
2022-04-02 23:42:05 +0530

    updates

--- script.js
+++ script.js
@@ -54,5 +54,13 @@ function deletingEffect(){
     
     typingEffect();
 
+//FORM SUBMIT MESSAGE
+    // const form = document.querySelector('form');
+    // const thankYouMessage = document.querySelector('#thank-you-message');
+    // form.addEventListener('submit', (e) => {
+    //   e.preventDefault();
+    //   thankYouMessage.classList.add('show');
+    //   setTimeout(() => form.submit(), 2000);
+    // });
 
-
+    // console.log(form)
