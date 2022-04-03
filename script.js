1da0e468f5fc40b98dfabfd17536af7b31c5e5e9
riyaverma338 <vermariya1202@gmail.com>
2022-04-03 18:12:57 +0530

    changes

--- script.js
+++ script.js
@@ -3,64 +3,63 @@ var scrolltoTopButton = document.getElementById("myBtn");
 
 scrolltoTopButton.addEventListener("click", scrollToTop);
 
-function scrollToTop(){
-
-window.scrollTo({
-    top:0,
-    behavior: "smooth"
-});
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 }
 
 //TYPING EFFECT
-const words = ["Hi, I'm Riya" , "I'm glad you're here!"];
-let i=0;
+const words = ["Hi, I'm Riya", "I'm glad you're here!"];
+let i = 0;
 let timer;
 function typingEffect() {
-    let head = words[i].split("");
-    var loopTyping = function() {
-        if(head.length > 0) {
-            document.getElementById('head').innerHTML += head.shift();
-        }
-        else {
-            deletingEffect();
-            return false;
-        };
-        timer = setTimeout(loopTyping, 250);
-    };
-    loopTyping();
+  let head = words[i].split("");
+  var loopTyping = function () {
+    if (head.length > 0) {
+      document.getElementById("head").innerHTML += head.shift();
+    } else {
+      deletingEffect();
+      return false;
+    }
+    timer = setTimeout(loopTyping, 250);
+  };
+  loopTyping();
 }
 
-function deletingEffect(){
-    let head = words[i].split("");
-    var loopDeleting = function() {
-        if (head.length > 0) {
-            head.pop();
+function deletingEffect() {
+  let head = words[i].split("");
+  var loopDeleting = function () {
+    if (head.length > 0) {
+      head.pop();
 
-            document.getElementById('head').innerHTML = head.join("");
-        }
-        else{
-                if (words.length > (i + 1)) {
-                    i++;
-                } else {
-                    i = 0;
-                };
-                typingEffect();
-                return false;
-            };
-            timer = setTimeout(loopDeleting, 50);
-        };
-        loopDeleting();
-    };
-    
-    typingEffect();
+      document.getElementById("head").innerHTML = head.join("");
+    } else {
+      if (words.length > i + 1) {
+        i++;
+      } else {
+        i = 0;
+      }
+      typingEffect();
+      return false;
+    }
+    timer = setTimeout(loopDeleting, 50);
+  };
+  loopDeleting();
+}
+
+typingEffect();
 
 //FORM SUBMIT MESSAGE
-    // const form = document.querySelector('form');
-    // const thankYouMessage = document.querySelector('#thank-you-message');
-    // form.addEventListener('submit', (e) => {
-    //   e.preventDefault();
-    //   thankYouMessage.classList.add('show');
-    //   setTimeout(() => form.submit(), 2000);
-    // });
+// const form = document.querySelector('form');
+// const thankYouMessage = document.querySelector('#thank-you-message');
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   thankYouMessage.classList.add('show');
+//   setTimeout(() => form.submit(), 2000);
+// });
 
-    // console.log(form)
+// console.log(form)
+
+
