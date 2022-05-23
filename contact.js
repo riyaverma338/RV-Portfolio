4b7f37e286dfd5e26cb14fc8e16267eacbd7ce85
riyaverma338 <vermariya1202@gmail.com>
2022-05-23 16:33:56 +0530

    update

--- contact.js
+++ contact.js
@@ -3,14 +3,13 @@ function sendEmail(){
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
         subject: document.getElementById("subject").value,
-        message: document.getElementById("message").value,
+        message: document.getElementById("msg").value,
     };
     emailjs.send('service_yhz4w54', 'template_39k4vj5', tempPara)
     .then(function(res){
         console.log("success",res.status);
     })
 }
-
 
 
 
